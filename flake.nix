7deb49fe5666ec5d9379b87653c3d9b26a76c1e3
Nor Führ <49118502+The1Penguin@users.noreply.github.com>
2025-07-02 10:21:38 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "OpenTESArena goes flakes";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    otesa = {
+      url = "github:afritz1/OpenTESArena";
+      flake = false;
+    };
+    JoltPhysics = {
+      url = "github:jrouwe/JoltPhysics?ref=tags/v5.3.0";
+      flake = false;
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, flake-parts, otesa, JoltPhysics, ... }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      perSystem = {self', pkgs, system, ...}: {
+        packages.default = self'.packages.otesa;
+        packages.otesa = pkgs.stdenv.mkDerivation {
+          pname = "otesa";
+          version = "0.16.0";
+
+          src = otesa;
+
+          patches = [
+            ./nix-fetch.patch
+          ];
+
+          nativeBuildInputs = with pkgs; [ cmake gnumake SDL2 SDL2.dev openal wildmidi ];
+        buildInputs = with pkgs; [ unrar-wrapper ];
+
+          JoltPhysics_SRC = JoltPhysics;
+          Arena_SRC = (pkgs.fetchzip {
+                          url = "https://cdnstatic.bethsoft.com/elderscrolls.com/assets/files/tes/extras/Arena106Setup.zip";
+                          sha256 = "sha256-g9Qakm3ZU/IytEKlPqF4e9OUdcywn1blMA4Or7RLmMM=";
+                          stripRoot = false;
+                        });
+          enableParallelBuilding = true;
+
+          buildPhase = ''
+            cp $Arena_SRC/Arena106.exe .
+            unrar x Arena106.exe data/
+            make -j $NIX_BUILD_CORES
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin/
+            cp otesa $out/bin/
+            cp -r data $out/bin/
+            cp -r options $out/bin/
+
+          '';
+        };
+
+      };
+    };
+}
